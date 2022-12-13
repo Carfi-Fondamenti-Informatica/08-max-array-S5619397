7369f408f9b93e60b11703269499c20127a2b448
S5619397 <118506564+S5619397@users.noreply.github.com>
2022-12-13 09:42:07 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,16 +1,1 @@
-#include <iostream>
-#include "lib.h"
-using namespace std;
 
-int main(){
-    int D;
-    cin>>D;
-    float num[D];
-    
-    for (int i=0;i< D;i++){
-        cin>> num[i];
-    }
-    cout<< funzione(num, D);
-    
-    return 0;
-}
